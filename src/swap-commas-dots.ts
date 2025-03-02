f3c8af5219f72d10b3381d461d8f7dee061bf5fd
Bruno Picinin Cardoso <brunopicinin@gmail.com>
2025-03-01 23:55:44 -0300

    Fix lint

--- src/swap-commas-dots.ts
+++ src/swap-commas-dots.ts
@@ -3,13 +3,13 @@ import { getSelectedText, Clipboard, showToast, Toast } from "@raycast/api";
 export default async () => {
   try {
     const selectedText = await getSelectedText();
-    const transformedText = selectedText.replace(/[\.,]/g, (val) => val === "." ? "," : ".");
+    const transformedText = selectedText.replace(/[.,]/g, (val) => (val === "." ? "," : "."));
     await Clipboard.paste(transformedText);
   } catch (error) {
     await showToast({
       style: Toast.Style.Failure,
       title: "Cannot transform text",
-      message: String(error)
+      message: String(error),
     });
   }
 };
