e37dac86de718409e14165fd4b06e22880400267
Bruno Picinin Cardoso <brunopicinin@gmail.com>
2025-03-06 08:42:38 -0300

    Improve error handling

--- src/swap-commas-dots.ts
+++ src/swap-commas-dots.ts
@@ -3,13 +3,17 @@ import { getSelectedText, Clipboard, showToast, Toast } from "@raycast/api";
 export default async () => {
   try {
     const selectedText = await getSelectedText();
-    const transformedText = selectedText.replace(/[.,]/g, (val) => (val === "." ? "," : "."));
+    const transformedText = transformText(selectedText);
     await Clipboard.paste(transformedText);
   } catch (error) {
     await showToast({
       style: Toast.Style.Failure,
       title: "Cannot transform text",
-      message: String(error),
+      message: error instanceof Error ? error.message : String(error),
     });
   }
 };
+
+function transformText(text: string) {
+  return text.replace(/[.,]/g, (val) => (val === "." ? "," : "."));
+}
